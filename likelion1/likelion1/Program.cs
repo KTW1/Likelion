0d5bfecfefd772ac1a158ef5d93769c9218fd2c5
OZNZ <85742380+KTW1@users.noreply.github.com>
2025-02-21 09:57:53 +0900

    tmp1

---
+++ likelion1/likelion1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace likelion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("git connection"); //출력2
+        }
+    }
+}
