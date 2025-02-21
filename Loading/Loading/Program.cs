764d5ee8627b97916d7d1ee6def3812f845ba367
OZNZ <85742380+KTW1@users.noreply.github.com>
2025-02-21 15:00:08 +0900

    loading

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start!");
+            Thread.Sleep(1000); //1000 per 1s
+            Console.Clear(); //화면 지우기
+            Console.WriteLine("□□□□□□□□");
+            Console.WriteLine("Downloading...");
+            Thread.Sleep(1000); //1000 per 1s
+            Console.Clear(); //화면 지우기
+            //Console.WriteLine("                ");
+            //Console.WriteLine("Downloading...");
+            //Thread.Sleep(1000); //1000 per 1s
+            //Console.Clear(); //화면 지우기
+            Console.WriteLine("■□□□□□□□");
+            Console.WriteLine("Downloading...");
+            Thread.Sleep(1000); //1000 per 1s
+            Console.Clear(); //화면 지우기
+            Console.WriteLine("■■□□□□□□");
+            Console.WriteLine("Downloading...");
+            Thread.Sleep(1000); //1000 per 1s
+            Console.Clear(); //화면 지우기
+            Console.WriteLine("■■■□□□□□");
+            Console.WriteLine("Downloading...");
+            Thread.Sleep(1000); //1000 per 1s
+            Console.Clear(); //화면 지우기
+            Console.WriteLine("■■■■□□□□");
+            Console.WriteLine("Downloading...");
+            Thread.Sleep(1000); //1000 per 1s
+            Console.Clear(); //화면 지우기
+            Console.WriteLine("■■■■■□□□");
+            Console.WriteLine("Downloading...");
+            Thread.Sleep(1000); //1000 per 1s
+            Console.Clear(); //화면 지우기
+            Console.WriteLine("■■■■■■□□");
+            Console.WriteLine("Downloading...");
+            Thread.Sleep(1000); //1000 per 1s
+            Console.Clear(); //화면 지우기
+            Console.WriteLine("■■■■■■■□");
+            Console.WriteLine("Downloading...");
+            Thread.Sleep(1000); //1000 per 1s
+            Console.Clear(); //화면 지우기
+            Console.WriteLine("■■■■■■■■");
+            Console.WriteLine("Compelete!");
+
+            //for{
+            //    int a, n;
+            //    a = 0;
+            //    a < n
+            //}
+        }
+    }
+}
