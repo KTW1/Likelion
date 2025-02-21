3883b002f7e96b1e17347750baac3b69ca6d2913
OZNZ <85742380+KTW1@users.noreply.github.com>
2025-02-21 17:59:51 +0900

    textNovel

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //로딩바 시작화면
+            Console.WriteLine("Press Enter Key");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("□□□□□♩");
+            Console.WriteLine("Wait Please...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■□□□□♬");
+            Console.WriteLine("Wait Please...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■□□□♪");
+            Console.WriteLine("Wait Please...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■□□♬");
+            Console.WriteLine("Wait Please...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■□♩");
+            Console.WriteLine("Wait Please...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■♪");
+            Console.WriteLine("Begin");
+            Console.Clear();
+
+            //게임스토리, 텍스트노벨?
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("ㅣ               제 1장                 ㅣ");
+            Console.ReadLine();
+            Console.WriteLine("ㅣ      작은 별빛 속에서 속삭이는       ㅣ");
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("밤하늘을 수놓은 별들이 저마다의 이야기를 속삭이는 고요한 밤, 당신은 비밀 정원을 발견합니다. ")
+            Console.WriteLine("정원 한가운데 자리 잡은 꽃 한 송이가 은은한 빛을 발하며 마치 말을 건네듯 향기를 풍깁니다.\r\n");
+            Console.WriteLine("이 꽃은 주인공의 마음속 깊이 자리 잡아 깊은 울림을 남깁니다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("ㅣ               제 2장                 ㅣ");
+            Thread.Sleep(1000);
+            Console.WriteLine("ㅣ        옅은 바람에도 흔들리는        ㅣ");
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("당신의 희망찬 노래가 마을 전체에 퍼져나가며 점점 유명해집니다.");
+            Console.WriteLine("하지만 예상치 못한 시련, 질투와 시기심에 눈먼 이들의 모함이 시작됩니다.");
+            Console.WriteLine("상처를 입지만, 노래로 사람들의 마음을 어루만지고자 마음 먹습니다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("ㅣ               제 3장                 ㅣ");
+            Thread.Sleep(1000);
+            Console.WriteLine("ㅣ     새벽 어스름, 달빛도 숨어있는     ㅣ");
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("어느 날, 길을 잃은 당신은 신비로운 노인을 만나 오랜 세월 간직해 온 비밀을 듣습니다.");
+            Console.WriteLine("그 비밀은 세상을 구할 전설 속 꽃, '달꽃'에 관한 것입니다");
+            Console.WriteLine("이 꽃은 순수한 마음을 지닌 자만이 찾을 수 있고, 그 힘으로 어둠을 물리칠 수 있다 합니다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("ㅣ               제 4장                 ㅣ");
+            Thread.Sleep(1000);
+            Console.WriteLine("ㅣ      그럼에도 결국 꽃은 개화하는     ㅣ");
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("달빛을 따라 수호자를 찾아 모험을 떠납니다.");
+            Console.WriteLine("갖은 어려움과 위험에 맞서며 포기하지 않고 계속 나아갑니다.");
+            Console.WriteLine("결국, 꽃을 찾아내 개화시키는 데 성공하고, 다시금 평화를 되찾습니다");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("ㅣ               제 5장                 ㅣ");
+            Thread.Sleep(1000);
+            Console.WriteLine("ㅣ       찬란한 햇빛 아래, 만발하는     ㅣ");
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("당신은 더 이상 작고 여린 존재가 아닙니다.");
+            Console.WriteLine("존경과 사랑을 받으며 자신의 정원에서 행복하게 살아갑니다.");
+            Console.WriteLine("당신의 노래가 세상에 영원히 울려 퍼지며 모두에게 희망과 용기를 전합니다");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Thank you for Playing");
+            Console.Clear();
+        }
+    }
+}
---
+++ gameStory/gameStory/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace gameStory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //로딩바 시작화면
+            Console.WriteLine("Press Enter Key");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("□□□□□♩");
+            Console.WriteLine("Wait Please...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■□□□□♬");
+            Console.WriteLine("Wait Please...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■□□□♪");
+            Console.WriteLine("Wait Please...");
+            Thread.Sleep(1000); 
+            Console.Clear();
+            Console.WriteLine("■■■□□♬");
+            Console.WriteLine("Wait Please...");
+            Thread.Sleep(1000); 
+            Console.Clear(); 
+            Console.WriteLine("■■■■□♩");
+            Console.WriteLine("Wait Please...");
+            Thread.Sleep(1000);
+            Console.Clear(); 
+            Console.WriteLine("■■■■■♪");
+            Console.WriteLine("Begin");
+            Console.Clear();
+
+            //게임스토리, 텍스트노벨?
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("ㅣ               제 1장                 ㅣ");
+            Console.ReadLine();
+            Console.WriteLine("ㅣ      작은 별빛 속에서 속삭이는       ㅣ");
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("ㅣ               제 2장                 ㅣ");
+            Console.ReadLine();
+            Console.WriteLine("ㅣ        옅은 바람에도 흔들리는        ㅣ");
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("ㅣ               제 3장                 ㅣ");
+            Console.ReadLine();
+            Console.WriteLine("ㅣ     새벽 어스름, 달빛도 숨어있는     ㅣ");
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("ㅣ               제 4장                 ㅣ");
+            Console.ReadLine();
+            Console.WriteLine("ㅣ      그럼에도 결국 꽃은 개화하는     ㅣ");
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("ㅣ               제 5장                 ㅣ");
+            Console.ReadLine();
+            Console.WriteLine("ㅣ       찬란한 햇빛 아래, 만발하는     ㅣ");
+            Console.WriteLine(" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Thank you for Playing");
+            Console.Clear();
+        }
+    }
+}
