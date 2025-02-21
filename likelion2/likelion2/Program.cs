64ad629cb775dd377226360d963edae046b2da33
OZNZ <85742380+KTW1@users.noreply.github.com>
2025-02-21 10:23:35 +0900

    2

---
+++ likelion2/likelion2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace likelion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수선언
+            int age; //정수형 변수를 선언
+            age = 10; //age라는 int변수에 25출력값 저장
+
+            Console.WriteLine(age); //age변수 저장된 값 출력
+            //ctrl F5
+        }
+    }
+}
