f0e8ce2ed4bd915e01dde78d922cc3a618ffe56e
OZNZ <85742380+KTW1@users.noreply.github.com>
2025-02-24 16:49:56 +0900

    week2_1th

---
+++ week2_2th/week2_2th/Program.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2_2th
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////전위와 후위 a++ ++a;
+            //int a = 3;
+            //Console.WriteLine("a = " + a++);
+
+            ////관계연산
+            //int x = 5, y = 10;
+            //Console.WriteLine(x < y);
+            //Console.WriteLine(x == y);
+            //Console.WriteLine(x != y);
+
+            ////3-11논리;
+            //bool a = true, b = false;
+            //Console.WriteLine(a && b);
+            //Console.WriteLine(a || b);
+            //Console.WriteLine(!a);
+
+            //a = !b;
+            //Console.WriteLine(a);
+
+            ////13비트연산
+            //int x = 5, y = 3; 0101 0011
+            //Console.WriteLine(x&y); 0001
+            //Console.WriteLine(x|y); 0111
+            //Console.WriteLine(x^y); 0110
+            //Console.WriteLine(~x); //
+
+
+            ////14시프트연산
+            //int value = 4;
+            //Console.WriteLine(value << 1);
+            //Console.WriteLine(value >> 1);
+
+            ////15기타연산, 삼항연산
+            //int a = 10, b = 20;
+            //int max = (a > b) ? a : b; //a>b? 맞으면t a, 틀리면f b.
+            //Console.WriteLine(max);
+
+            //Console.Write("??? : ");
+            //int key = int.Parse(Console.ReadLine());
+            //string res;
+            //res = (key = null) ? "open" : "close";
+
+            //Console.WriteLine(res);
+
+            ////우선순위.일반 연산
+            //int result = 10 + 2 * 5;
+            //Console.WriteLine(result);
+
+            //int adjustedResult = (10 + 2) * 5;
+            //Console.WriteLine(adjustedResult);
+
+            //int score = 69;
+            //if (score >= 90)
+            //{
+            //    Console.WriteLine("A");
+            //}
+            //else if (score > 70)
+            //{
+            //    Console.WriteLine("B");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("F");
+
+            //string ID = "SW";
+            //if(ID == "SW")
+            //{
+            //    Console.WriteLine("Login");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("fail");
+            //}
+
+            //int score =78;
+            //if (score >= 90)
+            //{
+            //    Console.WriteLine("A");
+            //}
+            //else if (score >= 80)
+            //{
+            //    Console.WriteLine("B");
+            //}
+            //else if (score >= 70)
+            //{
+            //    Console.WriteLine("C");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("F");
+            //}
+
+            //소지금을 입력, 0~100는 "무한의대검", 101~200 "카타나", 201~300 "진은검", 301~400 "집판검" 401~500 "엑스칼리버" 501~600 "유령검"
+
+            //Console.Write("소지금 입력: ");
+            //int pay = int.Parse(Console.ReadLine());
+            //string weapon = default;
+            //int att = 0;
+
+            //if (pay >= 0 && pay <= 100)
+            //{
+            //    weapon = "무한의 대검";
+            //    att = 1;
+            //}
+            //else if (pay >= 101 && pay <= 200)
+            //{
+            //    weapon = "카타나";
+            //    att = 2;
+            //}
+            //else if (pay >= 201 && pay <= 300)
+            //{
+            //    weapon = "진은검";
+            //    att = 3;
+            //}
+            //else if (pay >= 301 && pay <= 400)
+            //{
+            //    weapon = "집판검";
+            //    att = 4;
+            //}
+            //else if (pay >= 401 && pay <= 500)
+            //{
+            //    weapon = "엑스칼리버";
+            //    att = 5;
+            //}
+            //else if (pay >= 501 && pay <= 600)
+            //{
+            //    weapon = "유령검";
+            //    att = 6;
+            //}
+            //else
+            //{
+            //    weapon = "전설의 검";
+            //    att = 7;
+            //}
+
+            //Console.WriteLine("캐릭터 :멋사검존");
+            //Console.WriteLine($"무기 : {weapon}");
+            //Console.WriteLine($"공격력 : 100+{att}");
+
+            //            문제 1.세 정수의 최대값 구하기
+            //              문제 설명:
+            //            사용자로부터 3개의 정수를 입력받아 그 중 가장 큰 값을 출력하는 프로그램을 작성하세요.
+            //            요구사항:
+            //사용자에게 세 개의 정수를 입력받습니다.
+            //if문을 사용하여 가장 큰 정수를 결정합니다.
+            //결과를 “최대값: X” 형식으로 출력합니다.
+
+            Console.Write("1번째:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("2번째:");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("3번쨰:");
+            int c = int.Parse(Console.ReadLine());
+
+            int max;
+
+            if (a >= b && a >= c)
+            {
+                max = a;
+            }
+            else if (a <= b && b >= c)
+            {
+                max = b;
+            }
+            else
+            {
+                max = c;
+            }
+
+            Console.WriteLine("최대값:" + max);
+            Console.WriteLine();
+
+            Console.Write("학생의 점수를 입력하세요: ");
+            int score = int.Parse(Console.ReadLine());
+            string grade;
+            if (score >= 90)
+            {
+                grade = "A 학점";
+            }
+            else if (score >= 80)
+            {
+                grade = "B 학점";
+            }
+            else if (score >= 70)
+            {
+                grade = "C 학점";
+            }
+            else if (score >= 60)
+            {
+                grade = "D 학점";
+            }
+            else
+            {
+                grade = "F 학점";
+            }
+
+            Console.WriteLine($"학생의 점수: {score} = {grade}");
+
+            //문제 3.간단한 사칙연산 계산기
+            //문제 설명:
+            //사용자로부터 두 개의 숫자와 연산자(+, -, *, /)를 입력받아 해당 연산을 수행하고 결과를 출력하는 계산기 프로그램을 작성하세요.
+            //요구사항:
+
+            //두 개의 숫자와 연산자 기호를 입력받습니다.
+            //if문을 사용하여 연산자를 확인하고 해당 연산을 수행합니다.
+            //나눗셈의 경우 0으로 나누는 상황을 처리하여 에러 메시지를 출력합니다.
+            //결과는 “결과: X” 형식으로 출력합니다. 
+
+            Console.WriteLine();
+            Console.Write("첫 번째 숫자를 입력하세요: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("두 번째 숫자를 입력하세요: ");
+            int num2 = int.Parse(Console.ReadLine());
+ 
+            Console.Write("연산자를 입력하세요 (+, -, *, /): ");
+            char operation = char.Parse(Console.ReadLine());
+            Console.WriteLine();  
+
+            int result = 0;
+            bool validOperation = true;  
+
+            if (operation == '+')
+            {
+                result = num1 + num2;
+            }
+            else if (operation == '-')
+            {
+                result = num1 - num2;
+            }
+            else if (operation == '*')
+            {
+                result = num1 * num2;
+            }
+            else if (operation == '/')
+            {
+                if (num2 == 0)
+                {
+                    Console.WriteLine("error: 0으로 나눌 수 없습니다.");
+                    validOperation = false;  
+                }
+                else
+                {
+                    result = num1 / num2;
+                }
+            }
+            else
+            {
+                Console.WriteLine("error: 유효하지 않은 연산자입니다.");
+                validOperation = false;
+            }
+
+            if (validOperation)
+            {
+                Console.WriteLine($"결과: {result}");
+            }
+        }
+    }
+}
+
---
+++ week2nd/week2nd/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2nd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2진수 binary_ 0224Mon
+            //Console.WriteLine("2binary input : ");
+            //string binaryinput = Console.ReadLine();
+            //int decimalValue = Convert.ToInt32(binaryinput, 2);
+            //string binaryOutput = Convert.ToString(decimalValue, 2);
+
+            //Console.WriteLine($"입력한 2진수 : {binaryinput}");
+            //Console.WriteLine($"10진수 변환 : {decimalValue}");
+            //Console.WriteLine($"다시 2진수 변환 : {binaryOutput}");
+
+            //자동변수var string, char. 복잡해지면 곤란할 수 있다.
+            //var name = "Alice";
+            //var age = 'A';
+            //var isStudent = true;
+
+            //Console.WriteLine($"이름:{name},age:{age},student:{isStudent}");
+
+            //기본값default
+            //int defaultInt = default;
+            //string defaultString = default;
+            //bool defaultBool = default;
+
+            //Console.WriteLine($"정수 기본값: {defaultInt}");
+            //Console.WriteLine($"문자열 기본값: {defaultString}");
+            //Console.WriteLine($"논리 기본값: {defaultBool}");
+
+            //3.연산자? 전부 외우기보단 읽고 이해하는 중심.
+            //int a = 5.b = 3;
+            //int sum=a+b; 
+            //bool isEqual = (a == b);
+
+            //Console.WriteLine($"sum : {sum}");
+            //Console.WriteLine($"a and b same? {isEqual}");
+
+            //단항 연산자
+            //int number = 5;
+            //Console.WriteLine(+number);
+            //Console.WriteLine(-number);
+
+            ////변환 연산
+            //double pi = 3.14;
+            //int integerPi = (int)pi;
+
+            //Console.WriteLine(integerPi);
+
+            //int iKor = 90;
+            //int iMath = 75;
+            //int iEng = 88;
+            //int sum = iKor + iMath + iEng;
+            //float average = 0.0f;
+            //average = (float)sum / 3;
+            //Console.WriteLine("Score:" + sum);
+            //Console.WriteLine("Average:" + average);
+
+            //산술연산. %의 나머지연산이 중심;
+            //int a = 10, b = 3;
+            //Console.WriteLine(a+b);
+            //Console.WriteLine(a-b);
+            //Console.WriteLine(a*b);
+            //Console.WriteLine(a/b);
+            //Console.WriteLine(a % b);
+
+            //산술연산2
+            //int number = 7;
+            //int sum = 2;
+            //sum = number % 2;
+            //Console.WriteLine("홀짝판별 : " + sum);
+
+            ////산술3 랜덤가챠 기반
+            //bool isEqual = false;
+            //int a, b = 5;
+            //isEqual = (a == b);
+
+            //Console.WriteLine("a,b가 같은가?" + isEqual);
+
+            //문자열 연결연산
+            //string firstName = "Alice";
+            //string lastName = "Smith";
+
+            //Console.WriteLine(firstName+" "+lastName);
+
+            ////할당, 증감연산
+            //int a = 10;
+            //a += 5;
+            //Console.WriteLine(a);
+
+            //int b = 3;
+            //b++;
+            //Console.WriteLine(b);
+
+            //--b;
+            //Console.WriteLine(b);
+
+            //average += 5;
+            //average -= 5;
+            //average *= 5;
+            //average /= 5;
+            //average %= 5;
+            //Console.WriteLine(average);
+
+            ////오전복습문제1
+
+            //Console.WriteLine($"국어?");
+            //int korScore = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"영어?");
+            //int engScore = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"수학?");
+            //int mathSrore = int.Parse(Console.ReadLine());
+
+            //int score = korScore + engScore + mathSrore;
+            //float average = (float)score / 3;
+            //Console.WriteLine($"총점={score}, 평균= {average.ToString("F2")}");
+
+            ////문제2
+            ////Console.WriteLine("반전시킬 정수?");
+
+            //Console.WriteLine("");
+            //Console.WriteLine("정수?");
+            //int bit = int.Parse(Console.ReadLine());
+            //int rev = ~bit;
+            //Console.WriteLine($"비트반전=" + rev);
+
+            //    int[] scores = new int[3]; // 점수를 저장할 배열  
+            //  string[] subjects = { "국어", "영어", "수학" };  
+
+            //// 점수 입력  
+            //for (int i = 0; i < subjects.Length; i++)  
+            //{  
+            //    Console.Write($"{subjects[i]} 점수를 입력하세요: ");  
+            //    scores[i] = int.Parse(Console.ReadLine());  
+            //}  
+
+            //// 총합 및 평균 계산  
+            //int total = 0;  
+            //for (int i = 0; i < scores.Length; i++)  
+            //{  
+            //    total += scores[i];  
+            //}  
+
+            //double average = total / 3.0; // 실수로 나누기 위해 3.0 사용  
+
+            //Console.WriteLine($"총합: {total}");  
+            //Console.WriteLine($"평균: {average:F2}");
+
+        }
+    }
+}
